d0f4eeefe5092be928fe92da5191914095f509d3
Vadym8177 <vadim.8177@icloud.com>
2022-07-16 12:50:15 +0300

    reviews моб версия

--- src/slider.js
+++ src/slider.js
@@ -1,13 +1,13 @@
 new Swiper('.swiper', {
-  speed: 400,
+  speed: 600,
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
   },
-    // autoplay: {
-    //   delay: 3000,
-    //   stopOnLastSlide: false,
-    //   disableOnInteraction: false,
-    // },
+  // autoplay: {
+  //   delay: 3000,
+  //   stopOnLastSlide: false,
+  //   disableOnInteraction: false,
+  // },
 });
