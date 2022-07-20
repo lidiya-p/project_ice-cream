a903d73d372a1964c10f4255dd6935be59f94a7e
Vadym8177 <vadim.8177@icloud.com>
2022-07-20 09:42:54 +0300

    bugfix

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,23 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+    aboutRef: document.querySelector('.site-nav'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.aboutRef.addEventListener('click', aboutRefOpen);
+
+  function aboutRefOpen(event) {
+    if (event.target.nodeName !== 'A') {
+      return;
+    }
+    toggleModal();
+  }
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle('is-open');
   }
 })();
